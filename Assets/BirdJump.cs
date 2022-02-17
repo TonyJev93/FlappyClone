385e6e231cfc909ef4970dbae6804fac40197c54
JUN GYEWON <tonyjev93@gmail.com>
2022-02-18 01:07:23 +0900

    init

---
+++ Assets/BirdJump.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdJump : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Bird Jump");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update Bird Jump");
+    }
+}
